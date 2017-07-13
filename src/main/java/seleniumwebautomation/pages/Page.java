7e3e121502c599166846a03a3a2d70cd6a1c42de
Hassene Bouraoui <hassene.bouraoui@gmail.com>
2017-07-12 22:29:01 -0400

    test

---
+++ src/main/java/seleniumwebautomation/pages/Page.java
@@ -0,0 +1,31 @@
+package seleniumwebautomation.pages;
+
+import org.openqa.selenium.WebDriver;
+
+/*
+ * Abstract class representation of a Page in the UI. Page object pattern
+ * 
+ * @author Hassene Bouraoui
+ */
+public abstract class Page {
+
+	protected WebDriver webDriver;
+
+	/*
+	 * Constructor injecting the WebDriver interface
+	 * 
+	 * @param webDriver
+	 */
+	public Page(WebDriver webDriver) {
+		this.webDriver = webDriver;
+	}
+
+	public WebDriver getWebDriver() {
+		return webDriver;
+	}
+
+	public String getTitle() {
+		return webDriver.getTitle();
+	}
+
+}
